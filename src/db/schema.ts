adef82333a45b2e85cbd76d762cebeab74a88ee2
fecapark <fecapark@gmail.com>
2025-03-19 06:46:49 +0900

    :sparkles: messages 테이블 스키마 정의

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const messages = sqliteTable('messages', {
+  ts: text('ts').primaryKey().notNull(),
+  threadTs: text('threadTs').notNull(),
+  channel: text('channel').notNull(),
+  edited: integer('edited', { mode: 'boolean' }).notNull().default(false),
+  user: text('user', { mode: 'json' }).notNull(),
+  reactions: text('reactions', { mode: 'json' }),
+  files: text('files', { mode: 'json' }),
+});
