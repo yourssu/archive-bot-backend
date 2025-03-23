43df2686c58984c96a7d0b373634a4ad51bba039
fecapark <fecapark@gmail.com>
2025-03-24 07:14:39 +0900

    :sparkles: threads, channel 가져오기, 업로드 엔드포인트 구현

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,12 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import { drizzle } from 'drizzle-orm/d1';
 
-export const d1 = (db: D1Database) => drizzle(db);
+import * as schema from '@/db/schema';
+
+export const d1 = (db: D1Database) =>
+  drizzle(db, {
+    schema,
+  });
 
 export const s3 = new S3Client({
   region: process.env.S3_BUCKET_REGION,
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { eq } from 'drizzle-orm';
 import { Hono } from 'hono';
 import { stream } from 'hono/streaming';
 
 import { d1 } from '@/db';
-import { messages } from '@/db/schema';
+import { channels, messages, threads } from '@/db/schema';
 import { uploadFileMiddleware } from '@/middlewares/upload';
 import { ParseBodyFromSchema } from '@/types/schema';
 import { WorkersEnv } from '@/types/workers';
@@ -18,9 +19,101 @@ type HonoBindings = {
 
 const app = new Hono<HonoBindings>();
 
-app.get('/messages', async (c) => {
+app.get('/channels', async (c) => {
   const db = d1(c.env.DB);
-  const result = await db.select().from(messages).all();
+  const result = await db.select().from(channels).all();
+  return c.json(result);
+});
+
+app.get('/channel/:id', async (c) => {
+  const { id } = c.req.param();
+  const db = d1(c.env.DB);
+  const result = await db.select().from(channels).where(eq(channels.id, id));
+
+  if (result.length === 0) {
+    return c.json(handleError(new Error('올바르지 않은 채널 id에요.')), 400);
+  }
+
+  return c.json(result[0]);
+});
+
+app.post('/channel/add', async (c) => {
+  const body = await c.req.parseBody<ParseBodyFromSchema<typeof channels>>();
+
+  const db = d1(c.env.DB);
+
+  try {
+    await db.insert(channels).values(body).onConflictDoNothing().run();
+  } catch (e: unknown) {
+    return c.json(handleError(e), 400);
+  }
+
+  return c.text('OK');
+});
+
+app.get('/:channelId/threads', async (c) => {
+  const { channelId } = c.req.param();
+  const db = d1(c.env.DB);
+  const result = await db.query.threads.findMany({
+    where: eq(threads.channel, channelId),
+    with: {
+      head: true,
+      channel: true,
+    },
+  });
+  return c.json(result);
+});
+
+app.get('/thread/:id', async (c) => {
+  const { id } = c.req.param();
+  const db = d1(c.env.DB);
+
+  const result = await db.query.threads.findFirst({
+    where: eq(threads.ts, id),
+    with: {
+      head: true,
+      channel: true,
+    },
+  });
+
+  if (!result) {
+    return c.json(handleError(new Error('올바르지 않은 스레드 id에요.')), 400);
+  }
+
+  return c.json(result);
+});
+
+app.post('/thread/add', async (c) => {
+  const body = await c.req.parseBody<ParseBodyFromSchema<typeof threads>>();
+
+  const db = d1(c.env.DB);
+
+  try {
+    await db
+      .insert(threads)
+      .values(body)
+      .onConflictDoUpdate({
+        target: threads.ts,
+        set: body,
+      })
+      .run();
+  } catch (e: unknown) {
+    return c.json(handleError(e), 400);
+  }
+
+  return c.text('OK');
+});
+
+app.get('/:threadId/messages', async (c) => {
+  const { threadId } = c.req.param();
+  const db = d1(c.env.DB);
+  const result = await db.query.messages.findMany({
+    where: eq(messages.threadTs, threadId),
+    with: {
+      thread: true,
+      channel: true,
+    },
+  });
   return c.json(result);
 });
 
