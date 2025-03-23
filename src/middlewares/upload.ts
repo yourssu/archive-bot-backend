bfa84307138d328546464ce705b612465bda0560
fecapark <fecapark@gmail.com>
2025-03-23 23:40:17 +0900

    :sparkles: 파일 업로드 벨리데이션

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,33 +1,48 @@
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { createMiddleware } from 'hono/factory';
-import { v4 as uuidv4 } from 'uuid';
 
 import { s3 } from '@/db';
 import { MiddlewareVariables } from '@/types/middleware';
 import { handleError } from '@/utils/error';
 import { getExtension } from '@/utils/mime';
+import { checkObjectExists } from '@/utils/s3';
 
 export const uploadFileMiddleware = createMiddleware<MiddlewareVariables<{ key: string }>>(
   async (c, next) => {
-    const file = await c.req.blob();
+    const { file, id } = await c.req.parseBody();
+
+    if (!file || !(file instanceof File)) {
+      return c.json(handleError(new Error('file 필드가 없거나 올바르지 않아요.')), 400);
+    }
+
+    if (!id || typeof id !== 'string') {
+      return c.json(handleError(new Error('id 필드가 없거나 올바르지 않아요.')), 400);
+    }
 
     const extension = getExtension(file.type);
     if (!extension) {
       return c.json(handleError(new Error('지원하지 않는 확장자에요.')), 400);
     }
 
-    const key = `${uuidv4()}.${extension}`;
+    const key = `${id}.${extension}`;
     const buffer = Buffer.from(await file.arrayBuffer());
 
     try {
-      await s3.send(
-        new PutObjectCommand({
-          Bucket: process.env.S3_BUCKET_NAME,
-          Key: key,
-          Body: buffer,
-          ContentType: file.type,
-        })
-      );
+      const exists = await checkObjectExists({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: key,
+      });
+
+      if (!exists) {
+        await s3.send(
+          new PutObjectCommand({
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: key,
+            Body: buffer,
+            ContentType: file.type,
+          })
+        );
+      }
     } catch (e: unknown) {
       return c.json(handleError(e), 500);
     }
--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -7,6 +7,18 @@ import {
 
 import { s3 } from '@/db';
 
+export const checkObjectExists = async (params: HeadObjectCommandInput) => {
+  try {
+    await headObject(params);
+    return true;
+  } catch (e: unknown) {
+    if (e instanceof Error && e.name === 'NotFound') {
+      return false;
+    }
+    throw e;
+  }
+};
+
 export const getObject = async (params: GetObjectCommandInput) => {
   return s3.send(new GetObjectCommand(params));
 };
