e9bdb5980b1f6a18f1ed82356ca1a98a6bab0f5b
fecapark <fecapark@gmail.com>
2025-03-24 09:23:45 +0900

    :recycle: 스키마 타입 리팩토링

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -5,9 +5,5 @@ import { MapObjectValue, Prettify } from '@/types/misc';
 export type InferSchema<T extends AnySQLiteTable> = T['$inferInsert'];
 
 export type ParseBodyFromSchema<T extends AnySQLiteTable> = Prettify<
-  MapObjectValue<T['$inferInsert'], string>
->;
-
-export type ParseBodyFromSchemaSoft<T extends AnySQLiteTable> = Prettify<
-  MapObjectValue<T['$inferInsert'], string>
+  MapObjectValue<InferSchema<T>, string>
 >;
